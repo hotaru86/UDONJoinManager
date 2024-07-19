63f5f6b9baca16d3d49c758cb94fe7a153659b09
hotaru86 <131508602+hotaru86@users.noreply.github.com>
2024-07-19 11:58:09 +0900

    first

---
+++ JoinDevice.cs
@@ -0,0 +1,120 @@
+﻿/*
+Copyright (c) 2024 hotaru86
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+//ワールド内のユーザは1人1つこのJoinDeviceがアタッチされたオブジェクトのOwnerを保持する
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class JoinDevice : UdonSharpBehaviour
+{
+    //このJoinDeviceを保持しているユーザのID
+    //誰も保持していない場合は0
+    [UdonSynced, FieldChangeCallback(nameof(userID))] int _userID = 0;
+    
+    //例えば、このユーザがゲームにJoinしているかどうかを保持する
+    [UdonSynced, FieldChangeCallback(nameof(isJoin))] bool _isJoin = false;
+    
+    [SerializeField] JoinManager joinManager;
+
+    public int userID
+    {
+        get => _userID;
+        set
+        {
+            _userID = value;
+            OnUserIDCganged();
+        }
+    }
+
+    public bool isJoin
+    {
+        get => _isJoin;
+        set
+        {
+            _isJoin = value;
+            OnIsJoinChanged();
+        }
+    }
+
+
+    //userIDが変更された時の処理
+    public void OnUserIDCganged()
+    {
+        if (userID == Networking.LocalPlayer.playerId)
+        {
+            //userID本人のみのローカル処理
+            //userIDが自分のIDだった場合、このJoinDeviceを割り当てられたということなので、
+            //Ownerを取得する
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+        //全員の各自ローカル処理
+        joinManager.OnSomeoneUserIDChanged(userID);
+    }
+
+    //isJoinが変更された時の処理
+    public void OnIsJoinChanged()
+    {
+        if (userID == Networking.LocalPlayer.playerId)
+        {
+            //userID本人のみのローカル処理
+        }
+        //全員の各自ローカル処理
+        joinManager.OnSomeoneIsJoinedChanged(userID);
+    }
+
+    //このJoinDeviceを、playerIDがidの人に割り当てる処理
+    //実際にOwnerが変更されるのは、OnUserIDChangedの中
+    public void Attach(int id)
+    {
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        userID = id;
+        RequestSerialization();
+    }
+
+    //このJoinDeviceを割り当てから外す処理
+    public void Detach()
+    {
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        userID = 0;
+        isJoin = false;
+        RequestSerialization();
+    }
+
+    public void ToggleJoinState()
+    {
+        //このJoinDeviceが自分のものでない場合は何もしない
+        if (userID != Networking.LocalPlayer.playerId) return;
+        if (isJoin)
+        {
+            Leave();
+        }
+        else
+        {
+            Join();
+        }
+    }
+
+    public void Join()
+    {
+        if (userID != Networking.LocalPlayer.playerId) return;
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        isJoin = true;
+        RequestSerialization();
+    }
+    public void Leave()
+    {
+        if (userID != Networking.LocalPlayer.playerId) return;
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        isJoin = false;
+        RequestSerialization();
+    }
+}
---
+++ JoinManager.cs
@@ -0,0 +1,206 @@
+﻿/*
+Copyright (c) 2024 hotaru86
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class JoinManager : UdonSharpBehaviour
+{
+    //参加者のIDを保持する配列
+    [UdonSynced] public int[] playerIDs = new int[16];
+    //現在ワールドにいる人数を保持する変数
+    [UdonSynced] public int playerNum = 0;
+    //JoinDeviceをワールド上限人数分格納しておく
+    [SerializeField] public JoinDevice[] joinDevices;
+    private void Start()
+    {
+        DisplayJoinedPlayerList();
+        DisplayDebugText();
+    }
+
+    //誰かがJoinしたら、JoinDeviceを1つアタッチする
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        //JoinManagerのOwnerのみが処理を行う
+        if (!Networking.IsOwner(gameObject)) return;
+        //空きJoinDeviceを検索
+        var jd = GetJoinDeviceById(0);
+        if (jd == null) return;
+        jd.Attach(player.playerId);
+    }
+
+    //誰かがインスタンスを離れたら、JoinDeviceをデタッチする
+    public override void OnPlayerLeft(VRCPlayerApi player)
+    {
+        //JoinManagerのOwnerのみが処理を行う
+        if (!Networking.IsOwner(gameObject)) return;
+        var jd = GetJoinDeviceById(player.playerId);
+        if (jd == null) return;
+        jd.Detach();
+    }
+
+    //いずれかのJoinDeviceのuserIDが変更されたら呼ばれる
+    public void OnSomeoneUserIDChanged(int IDAfterChanged)
+    {
+        DisplayDebugText();
+    }
+
+    //いずれかのJoinDeviceのisJoinが変更されたら呼ばれる
+    public void OnSomeoneIsJoinedChanged(int IDAfterChanged)
+    {
+        if (Networking.IsOwner(gameObject))
+        {
+            //Ownerのみが処理
+            //playerIDsを更新
+            RefreshPlayerIDs();
+            
+            //Owner以外はOnDeserializationで以下の処理を行う
+            DisplayJoinedPlayerList();
+            DisplayDebugText();
+        }
+    }
+
+    //指定したIDをuserIDに持つJoinDeviceを検索して返す
+    public JoinDevice GetJoinDeviceById(int id)
+    {
+        foreach (JoinDevice jd in joinDevices)
+        {
+            if (jd.userID == id)
+            {
+                return jd;
+            }
+        }
+        return null;
+    }
+
+    //自分のplayerIDのplayerIDs内でのインデックスを取得
+    public int GetMyIndex()
+    {
+        int myID = Networking.LocalPlayer.playerId;
+        for (int i = 0; i < playerIDs.Length; i++)
+        {
+            if (playerIDs[i] == myID)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    
+    public override void OnDeserialization()
+    {
+        DisplayJoinedPlayerList();
+        DisplayDebugText();
+    }
+
+    //デバッグ用
+    [SerializeField] Text joinedPlayerListText;
+    public void RefreshPlayerIDs()
+    {
+        //直前の参加者リストを見て、抜けた人を探す
+        for (int i = 0; i < playerIDs.Length; i++)
+        {
+            if (playerIDs[i] == 0) continue;
+            //参加済みプレイヤーのJDを検索
+            var tmp = GetJoinDeviceById(playerIDs[i]);
+            if (tmp == null) continue;
+            //参加しなくなってたら
+            if (!tmp.isJoin)
+            {
+                //参加者リストから抜いて0にする
+                playerIDs[i] = 0;
+            }
+        }
+        //新しく参加した人を探す
+        foreach (JoinDevice jd in joinDevices)
+        {
+            //参加してる人を探す
+            if (jd.isJoin)
+            {
+                //直前の参加者リストにもう入ってるかを確認
+                bool alreadyJoined = false;
+                for (int i = 0; i < playerIDs.Length; i++)
+                {
+                    if (jd.userID == playerIDs[i])
+                    {
+                        alreadyJoined = true;
+                        break;
+                    }
+                }
+                //参加希望してるのにまだ入ってない
+                if (!alreadyJoined)
+                {
+                    //空きがあったら入れる
+                    for (int i = 0; i < playerIDs.Length; i++)
+                    {
+                        if (playerIDs[i] == 0)
+                        {
+                            playerIDs[i] = jd.userID;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        //playerNumを更新
+        playerNum = 0;
+        for (int i = 0; i < playerIDs.Length; i++)
+        {
+            if (playerIDs[i] != 0)
+            {
+                playerNum++;
+            }
+        }
+        RequestSerialization();
+    }
+
+    public void DisplayJoinedPlayerList()
+    {
+        if(joinedPlayerListText == null) return;
+        string str = "JoinedPlayers\n";
+        for (int i = 0; i < playerIDs.Length; i++)
+        {
+            int id = playerIDs[i];
+            if (id == 0) continue;
+            str += $"{id}\t{VRCPlayerApi.GetPlayerById(id).displayName}\n";
+        }
+        joinedPlayerListText.text = str;
+    }
+
+    //デバッグ用テキスト表示
+    [SerializeField] Text debugText;
+    void DisplayDebugText()
+    {
+        if (debugText == null) return;
+        string str = "";
+        str += "playerIDs\ns";
+        foreach (int id in playerIDs)
+        {
+            str += $"{id}\t";
+        }
+        str += "\n";
+        str += "\n";
+
+        for (int i = 0; i < joinDevices.Length; i++)
+        {
+            str += $"{i}\t";
+            str += $"{joinDevices[i].userID}\t";
+            str += $"J:{joinDevices[i].isJoin}\t";
+            var ownerPlayer = Networking.GetOwner(joinDevices[i].gameObject);
+            str += $"{ownerPlayer.displayName}({ownerPlayer.playerId})";
+            str += "\n";
+        }
+
+
+        debugText.text = str;
+    }
+
+
+}
